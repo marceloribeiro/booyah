4c38709b986361bc44b2e965256224b6892c2e05
Marcelo Ribeiro <themarceloribeiro@gmail.com>
2023-08-15 20:43:14 +0100

    Removing bad committed stuff

--- src/db/adapters/postgresql_adapter.py
+++ src/db/adapters/postgresql_adapter.py
@@ -1,11 +1,5 @@
 import psycopg
-import json
 import os
-
-GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
-STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
-
 
 class PostgresqlAdapter:
     @staticmethod
