70a61b42c00800ecfbcc16b2031da9fe1c561765
Marcelo Ribeiro <themarceloribeiro@gmail.com>
2023-09-04 11:26:51 -0300

    Fixing missing content type in response

--- src/booyah/helpers/request_format_helper.py
+++ src/booyah/helpers/request_format_helper.py
@@ -10,23 +10,30 @@ class ContentType(Enum):
 
 class RequestFormatHelper:
     def __init__(self, environment):
+        self.environment = environment
         if 'HTTP_ACCEPT' in environment:
             self.http_accept = environment['HTTP_ACCEPT']
         else:
             raise ValueError("Missing http accept header")
-    
+
     def respond_to(self, html=None, json=None, text=None):
         if html is not None and ContentType.HTML.value in self.http_accept:
+            self.environment['CONTENT_TYPE'] = ContentType.HTML.value
             return html()
         if json is not None and ContentType.JSON.value in self.http_accept:
+            self.environment['CONTENT_TYPE'] = ContentType.JSON.value
             return json()
         if text is not None and ContentType.TEXT.value in self.http_accept:
+            self.environment['CONTENT_TYPE'] = ContentType.TEXT.value
             return text()
-        
+
         # if not included in accept, will return prior argument
         if html is not None:
+            self.environment['CONTENT_TYPE'] = ContentType.HTML.value
             return html()
         if json is not None:
+            self.environment['CONTENT_TYPE'] = ContentType.JSON.value
             return json()
         if text is not None:
+            self.environment['CONTENT_TYPE'] = ContentType.TEXT.value
             return text()
