6a80e435deaaa688ae763a78884d4d6b8a4e3ffb
klaus-trabalhando <klaus.trabalhando@gmail.com>
2023-09-05 09:15:17 -0300

    New missed route matcher tests

--- src/tests/lib/router/route_matcher_test.py
+++ src/tests/lib/router/route_matcher_test.py
@@ -1,6 +1,8 @@
 from booyah.router.route_matcher import RouteMatcher
 
 class TestRouteMatcher:
+    def test_valid_home_route(self):
+        assert RouteMatcher('/').is_valid_url('/') == True
 
     def test_valid_route_param(self):
         assert RouteMatcher('/users/<int:id>').is_valid_url('/users/1') == True
@@ -16,6 +18,9 @@ class TestRouteMatcher:
 
     def test_exact_match_not_valid(self):
         assert RouteMatcher('/users/new').is_valid_url('/users/1') == False
+    
+    def test_extract_blank_params(self):
+        assert RouteMatcher('/users/new').build_params('/users/new') == { }
     
     def test_extract_param_int(self):
         assert RouteMatcher('/users/<int:id>').build_params('/users/1') == { "id": 1 }
