f285f65a74a3ca13fa032f1f439154b9e1c2d3eb
Klaus Trabalhando <klaus.trabalhando@gmail.com>
2023-12-11 12:41:01 -0300

    Booyah framework config file (#72)
    * Working on config file
    
    * Added application.yml to generators, created a way to put env vars in yml file, working on test
    
    * Finished tests
    
    * Changed method name

---
+++ src/booyah/framework.py
@@ -0,0 +1,55 @@
+import os
+import confuse
+from booyah.extensions.string import String
+from py_dotenv import read_dotenv
+
+with open('.booyah_version', 'r') as version_file:
+    booyah_version = version_file.read().strip()
+
+class Booyah:
+    root = os.getcwd()
+    folder_name = os.path.basename(os.getcwd())
+    name = String(os.path.basename(os.getcwd())).titleize()
+    version = booyah_version
+
+    @classmethod
+    def substitute_env_variables(self, config):
+        for key, value in config.items():
+            real_value = value.get()
+            if isinstance(real_value, dict):
+                Booyah.substitute_env_variables(value)
+            elif isinstance(real_value, str):
+                config[key] = os.path.expandvars(real_value)
+    
+    @classmethod
+    def config_to_dict(self, config):
+        result = {}
+        for key, value in config.items():
+            real_value = value.get()
+            if isinstance(real_value, dict):
+                result[key] = Booyah.config_to_dict(value)
+            else:
+                result[key] = real_value
+        return result
+
+    @classmethod
+    def configure(self):
+        read_dotenv('.env')
+        self.getenv = os.getenv
+        self.environment = os.getenv('BOOYAH_ENV')
+        self.is_development = self.environment == 'development'
+        self.is_test = self.environment == 'test'
+        self.is_production = self.environment == 'production'
+
+        config = confuse.Configuration(Booyah.name.classify())
+        config.set_file(os.path.join(Booyah.root, 'config', 'application.yml'))
+        Booyah.substitute_env_variables(config)
+        config_dict = Booyah.config_to_dict(config)
+
+        if self.environment and self.environment in config_dict:
+            self.env_config = config_dict[self.environment]
+        else:
+            self.env_config = {}
+        self.config = config_dict
+
+Booyah.configure()
---
+++ src/tests/lib/framework_test.py
@@ -0,0 +1,120 @@
+import os
+import tempfile
+import unittest
+import shutil
+from booyah.extensions.string import String
+
+PROD_DB_PASSWORD = "pwd_from_env"
+DEFAULT_DB_PASSWORD = "default_pwd"
+BOOYAH_ENV = 'test'
+TEST_DB_HOST = "host_test"
+
+class TestFramework(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.previous_dir = os.getcwd()
+        self.temp_dir = tempfile.mkdtemp()
+        os.chdir(self.temp_dir)
+        self.folder_name = os.path.basename(self.temp_dir)
+        self.create_version_file()
+        self.create_env_file()
+        self.create_application_yaml()
+    
+    @classmethod
+    def create_version_file(self):
+        file_path = os.path.join(self.temp_dir, '.booyah_version')
+        with open(file_path, 'w') as file:
+            file.write("99.99.99")
+
+    @classmethod
+    def create_env_file(self):
+        file_path = os.path.join(self.temp_dir, '.env')
+        with open(file_path, 'w') as file:
+            file.write(f"""
+DB_PASSWORD={PROD_DB_PASSWORD}
+BOOYAH_ENV={BOOYAH_ENV}
+            """)
+    
+    @classmethod
+    def create_application_yaml(self):
+        yaml_content = f"""
+defaults:
+    database: &database
+        host: localhost
+        port: 5432
+        adapter: postgresql
+        database: booyah
+        username: postgres
+        password: {DEFAULT_DB_PASSWORD}
+test:
+    database:
+        <<: *database
+        host: {TEST_DB_HOST}
+        port: 5432
+        adapter: postgresql
+        database: booyah
+development:
+    database:
+        <<: *database
+        host: localhost
+        port: 5432
+        adapter: postgresql
+        database: booyah
+production:
+    database:
+        <<: *database
+        host: $DB_HOST
+        port: 5432
+        adapter: postgresql
+        database: booyah_production
+        username: $DB_USERNAME
+        password: $DB_PASSWORD
+        """
+
+        self.config_folder = os.path.join(self.temp_dir, 'config')
+        os.makedirs(self.config_folder)
+
+        file_path = os.path.join(self.config_folder, 'application.yml')
+        with open(file_path, 'w') as file:
+            file.write(yaml_content)
+
+    @classmethod
+    def tearDownClass(self):
+        os.chdir(self.previous_dir)
+        if os.path.exists(self.temp_dir):
+            shutil.rmtree(self.temp_dir)
+    
+    def test_get_version(self):
+        from booyah.framework import Booyah
+        assert Booyah.version == "99.99.99"
+    
+    def test_root(self):
+        from booyah.framework import Booyah
+        assert self.temp_dir in Booyah.root
+    
+    def test_name(self):
+        from booyah.framework import Booyah
+        assert Booyah.name == String(os.path.basename(self.temp_dir)).titleize()
+    
+    def test_anchor_yaml(self):
+        from booyah.framework import Booyah
+        assert Booyah.config['development']['database']['password'] == DEFAULT_DB_PASSWORD
+    
+    def test_env_in_yaml(self):
+        from booyah.framework import Booyah
+        assert Booyah.config['production']['database']['password'] == PROD_DB_PASSWORD
+    
+    def test_environment(self):
+        from booyah.framework import Booyah
+        assert Booyah.environment == BOOYAH_ENV
+
+    def test_is_environment(self):
+        from booyah.framework import Booyah
+        assert Booyah.is_test == True
+        assert Booyah.is_development == False
+        assert Booyah.is_production == False
+    
+    def test_env_config(self):
+        from booyah.framework import Booyah
+        assert Booyah.env_config['database']['host'] == TEST_DB_HOST
